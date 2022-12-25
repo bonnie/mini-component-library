237527112e99536bc277c353bfb98c0e9bb19b63
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-12-24 18:09:27 -0800

    Write Select button workshop code

--- src/components/Select/Select.helpers.js
+++ src/components/Select/Select.helpers.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
 
-export function getDisplayedValue(value, children) {
+export function getDisplayedValueWidth(value, children, font) {
   const childArray = React.Children.toArray(children);
-  const selectedChild = childArray.find(
-    (child) => child.props.value === value
-  );
+  const selectedChild = childArray.find((child) => child.props.value === value);
 
-  return selectedChild.props.children;
+  const displayedText = selectedChild.props.children;
+  return getTextWidth(displayedText, font);
+}
+
+function getTextWidth(text, font) {
+  /* adapted from https://stackoverflow.com/a/58705306 */
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+
+  context.font = font || getComputedStyle(document.body).font;
+  console.log("context.font", context.font);
+
+  return context.measureText(text).width;
 }
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,84 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValueWidth } from "./Select.helpers";
 
+const height = `${43 / 16}rem`;
+const padding = `${12 / 16}rem ${18 / 16}rem ${12 / 16}rem ${16 / 16}rem`;
+
+/* adapted from 
+  - https://jonasfaehrmann.com/writing/post/2020-04-02-html-lessselectgreater-with-dynamic-size/
+  - https://www.w3docs.com/tools/code-editor/2709 
+*/
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+  const displayedValueWidth = getDisplayedValueWidth(
+    value,
+    children,
+    "1rem roboto, sans-serif"
+  );
+
+  // all in rem
+  const paddingWidthX = (18 + 16) / 16;
+  const dropArrowWidth = 12 / 16;
+  const selectWidth = displayedValueWidth / 16 + paddingWidthX + dropArrowWidth;
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectWrapper>
+      <DropArrow style={{ "--width": dropArrowWidth + "rem" }}>
+        <Icon size={24} id="chevron-down"></Icon>
+      </DropArrow>
+      <StyledSelect
+        style={{
+          "--height": height,
+          "--width": selectWidth + "rem",
+          "--padding": padding,
+          "--after-width": dropArrowWidth + "rem",
+        }}
+        value={value}
+        onChange={onChange}
+      >
+        {children}
+      </StyledSelect>
+    </SelectWrapper>
   );
 };
+
+const SelectWrapper = styled.span`
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const DropArrow = styled.span`
+  position: absolute;
+  right: 18px;
+  top: -2px; /* why -2px? bc of top padding in container? */
+  width: var(--width);
+  pointer-events: none; /* allows for clicks to go through to select element sitting behind */
+`;
+
+const StyledSelect = styled.select`
+  // remove default dropdown arrow
+  //   reference: https://www.w3docs.com/snippets/css/how-to-style-a-select-box-drop-down-with-only-css.html
+  appearance: none;
+
+  height: var(--height);
+  width: var(--width);
+  padding: var(--padding);
+  border: none;
+  border-radius: 0.5rem;
+
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700}; /* why doesn't this inherit from SelectWrapper? */
+
+  ${SelectWrapper}:hover & {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
